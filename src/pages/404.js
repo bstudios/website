b2e0f37d1cc98a6e143e321ca415b646d8b12acf
James Bithell <james@bithell.com>
2021-03-31 15:40:01 +0100

    Tweak 404 page style

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,14 @@
 import React from "react";
 
 import Layout from "../components/Layout";
+import styled from "styled-components";
 
+const Text = styled.p`
+  text-align: center;
+`;
 const NotFoundPage = () => (
   <Layout title="404 Page Not Found">
-    <p>Sorry - the page you requested couldn&apos;t be found</p>
+    <Text>Sorry - the page you requested couldn&apos;t be found</Text>
   </Layout>
 );
 
