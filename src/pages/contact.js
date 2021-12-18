5874f15518e40399d4f21230f9d968c7f46fc623
James Bithell <Jbithell@users.noreply.github.com>
2021-12-18 10:43:04 +0000

    Add service status link + support ticket link

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,16 +2,26 @@ import React from "react";
 
 import Layout from "../components/Layout";
 import styled from "styled-components";
+import Button from "../components/Button";
 
 const Text = styled.p`
   text-align: center;
-`;
+`; 
+// TODO come and tidy up the center tag!
 const ContactPage = () => (
   <Layout title="Contact Us">
     <Text>
       To get in touch drop us an email at studios@jbithell.com, and we&apos;ll get back to you as
       soon as we can!
     </Text>
+    <center>
+      <Button to="https://bstudios.freshdesk.com/support/tickets/new" primary={true}>
+        Create Support Ticket
+      </Button><br/>
+      <Button to="https://status.bithell.studio/">
+        Service Status Page
+      </Button>
+    </center>
   </Layout>
 );
 
