f52886fb8163e5099541b98d03cddd551b4c98d4
James Bithell <Jbithell@users.noreply.github.com>
2024-05-12 18:08:33 +0100

    Remove old site, move to mantine

---
+++ gatsby-config.ts
@@ -0,0 +1,32 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Bithell Studios`,
+    titleTemplate: "%s | Bithell Studios Ltd.",
+    description:
+      "Bithell Studios is a creative development studio based in the UK run by James Bithell",
+    author: "bstudios",
+    url: "https://bithell.studios",
+    image: "/images/defaultImage.png",
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-google-gtag", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, "gatsby-transformer-remark", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
+
+export default config;
